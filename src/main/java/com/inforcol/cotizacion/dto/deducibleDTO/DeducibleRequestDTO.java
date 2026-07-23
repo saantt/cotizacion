2110029c70f0b6d753b90b6f05224291a13b6b6c
Santiago Gomez Marin <sgmarin@inforcol.local>
2026-07-23 15:20:10 -0500

    feat: arreglo dto

--- src/main/java/com/inforcol/cotizacion/dto/deducibleDTO/DeducibleRequestDTO.java
+++ src/main/java/com/inforcol/cotizacion/dto/deducibleDTO/DeducibleRequestDTO.java
@@ -1,4 +1,4 @@
-package com.inforcol.cotizacion.DTO.deducibleDTO;
+package com.inforcol.cotizacion.dto.deducibleDTO;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Max;
