dcdaf12cdc5d4c73fe5952e7155c27d9b9c6e2ea
Santiago Gomez Marin <sgmarin@inforcol.local>
2026-07-30 10:48:16 -0500

    feat: se hicieron los Query customs

--- src/main/java/com/inforcol/cotizacion/repository/DeducibleRepository.java
+++ src/main/java/com/inforcol/cotizacion/repository/DeducibleRepository.java
@@ -3,6 +3,21 @@ package com.inforcol.cotizacion.repository;
 import com.inforcol.cotizacion.model.Deducible;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DeducibleRepository extends JpaRepository<Deducible,Long> {
+import java.util.List;
+
+public interface DeducibleRepository extends JpaRepository<Deducible, Long> {
+
+    List<Deducible> findByPorcentaje(Integer porcentaje);
+
+    List<Deducible> findByPorcentajeGreaterThan(Integer porcentaje);
+
+    List<Deducible> findByMontoMinimoGreaterThan(Double monto);
+
+    List<Deducible> findByPorcentajeBetween(Integer inicio, Integer fin);
+
+    boolean existsByPorcentaje(Integer porcentaje);
+
+    boolean existsByPorcentajeAndMontoMinimo(Integer porcentaje,
+                                             Double montoMinimo);
 
 }
