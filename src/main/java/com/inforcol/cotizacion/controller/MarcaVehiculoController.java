6393e318828f63aa0020df2b4bc872f2e2d487a4
Jose Luis Restrepo Gesamá <lgesama@inforcol.local>
2026-07-30 09:24:16 -0500

    feat: se configuró la conexión con el Front

--- src/main/java/com/inforcol/cotizacion/controller/MarcaVehiculoController.java
+++ src/main/java/com/inforcol/cotizacion/controller/MarcaVehiculoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/marcavehiculo")
+@CrossOrigin(origins = "http://localhost:4200")
 @Slf4j
 public class MarcaVehiculoController {
 
