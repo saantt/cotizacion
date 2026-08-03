ac328fb6edd13277b6dc322699a4a121930d51ad
Camila Orozco Salazar <csalazar@inforcol.local>
2026-08-03 10:49:02 -0500

    fix: CrossOrigin - Coverage

--- src/main/java/com/inforcol/cotizacion/controller/CoverageController.java
+++ src/main/java/com/inforcol/cotizacion/controller/CoverageController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ import lombok.extern.slf4j.Slf4j;
         name = "Coverage",
         description = "API for coverage management."
 )
+@CrossOrigin("http://localhost:4200")
 public class CoverageController {
 
     @Autowired
